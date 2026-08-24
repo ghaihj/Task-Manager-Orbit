e176f3d2958594b2ab488e0a8eeec5d9d25cb4f2
Ghaith <ghaithka82@gmail.com>
2026-08-24 19:40:36 +0300

    fix code

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,12 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::controller(AuthController::class)->group(function () {
+    Route::post('/register', 'register');
+    Route::post('/login', 'login');
+    Route::post('/logout', 'logout')->middleware('auth:sanctum');
+    Route::post('/me', 'me')->middleware('auth:sanctum');
+});
