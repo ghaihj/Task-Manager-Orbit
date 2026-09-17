d92a70136f41b82ece9b13a0bb81b20d1ee84c4e
Ghaith <ghaithka82@gmail.com>
2026-09-18 01:11:52 +0300

    fix code

--- routes/api.php
+++ routes/api.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Route;
 Route::controller(AuthController::class)->group(function () {
     Route::post('/register', 'register');
     Route::post('/login', 'login');
-    Route::post('/forgot-password', 'forgotPassword')->name('password.forgot');
+    Route::post('/forgot-password', 'forgotPassword')->name('password.reset');
     Route::post('/reset-password', 'resetPassword');
     Route::post('/logout', 'logout')->middleware('auth:sanctum');
     Route::post('/me', 'me')->middleware('auth:sanctum');
